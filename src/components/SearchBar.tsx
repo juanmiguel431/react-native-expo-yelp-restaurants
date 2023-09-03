7885cd2c537bbaf4585af513ede326850baa017f
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-02 20:26:22 -0400

    SearchBar expo icon added.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 interface SearchBarProps {
 }
 
 const SearchBar: React.FC<SearchBarProps> = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" size={30}/>
+      <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: '#d9d4d4',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1
   }
 });
 
